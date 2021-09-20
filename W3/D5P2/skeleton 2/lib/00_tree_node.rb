a792475fa59e901c501e333945b6c2b3957b7b57
alexlolas <alexanderlolas@yahoo.com>
2021-09-20 11:48:17 -0700

    First Phase PolyTreeNode

--- W3/D5P2/skeleton 2/lib/00_tree_node.rb
+++ W3/D5P2/skeleton 2/lib/00_tree_node.rb
@@ -1,16 +1,45 @@
+
 class PolyTreeNode
+
   attr_reader  :children, :parent
   attr_accessor :value
-  def initialize(value)
+  def initialize(value, children = [])
     @value = value
     @parent = nil
-    @children = []
+    @children = children
   end
 
-
-  def parent=(value)
-    @parent = value
+  def inspect
+    "#<Node: @value=#{value}, @children=#{children}, @parent=#{parent}>"
   end
 
- 
+  def parent=(node)
+    self.parent.children.delete(self) if self.parent
+    @parent = node
+    if @parent != nil
+      if !@parent.children.include?(self)
+        @parent.children << self
+      end
+    end
+    
+  end
+
+  def add_child(node)
+    if !@children.include?(node)
+      node.parent = self
+    else
+
+    end
+  end
+
+  def remove_child(child)
+    if self.children.include?(child)
+      child.parent = nil
+    else
+      raise 'error'
+    end
+    # child.parent = nil
+    # raise 'ree' unless children.include?(child)
+  end
+
 end
