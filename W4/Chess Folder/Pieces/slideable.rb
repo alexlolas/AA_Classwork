99055790d62eb250fa228779c3d7bc1769e3bf64
darothmedia <87622162+darothmedia@users.noreply.github.com>
2021-09-22 14:00:01 -0700

    Steppable Slideable [ar]

--- W4/Chess Folder/Pieces/slideable.rb
+++ W4/Chess Folder/Pieces/slideable.rb
@@ -4,7 +4,7 @@ module Slidable
     row = self.pos[0]
     
     @board.grid[row].each do |space| 
-      if space.is_a?(NullPiece)
+      if space.is_a?(NullPiece) || self.color != space.color
         hor << space
       end
     end
@@ -16,7 +16,7 @@ module Slidable
     trans = @board.transpose
     col = self.pos[1]
     trans.grid[col].each do |space| 
-      if space.is_a?(NullPiece)
+      if space.is_a?(NullPiece) || self.color != space.color
         vert << space
       end
     end
@@ -28,7 +28,7 @@ module Slidable
     row_idx = self.pos[0]
     col_idx = self.pos[1]
     until row_idx == 7 || col_idx == 7
-      if @board.grid[row_idx][col_idx].is_a?(NullPiece)
+      if @board.grid[row_idx][col_idx].is_a?(NullPiece) || self.color != @board.grid[row_idx][col_idx].color
         diag << [row_idx, col_idx]
       end
       row_idx += 1
@@ -36,7 +36,7 @@ module Slidable
     end
 
     until row_idx == 0 || col_idx == 0
-      if @board.grid[row_idx][col_idx].is_a?(NullPiece)
+      if @board.grid[row_idx][col_idx].is_a?(NullPiece) || self.color != @board.grid[row_idx][col_idx].color
         diag << [row_idx, col_idx]
       end
       row_idx -= 1
--- W4/Chess Folder/Pieces/steppable.rb
+++ W4/Chess Folder/Pieces/steppable.rb
@@ -11,9 +11,37 @@ module Steppable
       [[self.pos[0] - 1], [self.pos[1] + 1]],
     ]
 
+    valid_moves = []
+
+    arr.each do |space|
+      if (@board.grid[space[0]][space[1]].is_a?(NullPiece)) && ((space[0] > 0 && space[1] > 0) && (space[0] < 8 && space[1] < 8))
+        valid_moves << space
+      elsif self.color != @board.grid[space[0]][space[1]].color && ((space[0] > 0 && space[1] > 0) && (space[0] < 8 && space[1] < 8))
+      end
+    end
+    valid_moves
   end
 
   def knight
+    arr = [
+      [[self.pos[0] + 2], [self.pos[1] + 1]], 
+      [[self.pos[0] + 1], [self.pos[1] + 2]], 
+      [[self.pos[0] + 1], [self.pos[1] - 2]], 
+      [[self.pos[0] - 2], [self.pos[1] - 1]], 
+      [[self.pos[0] - 2], [self.pos[1] + 1]],
+      [[self.pos[0] - 1], [self.pos[1] + 2]],
+      [[self.pos[0] - 1], [self.pos[1] - 2]],
+      [[self.pos[0] + 2], [self.pos[1] - 1]],
+    ]
+
+    valid_moves = []
+
+    arr.each do |space|
+      if (@board.grid[space[0]][space[1]].is_a?(NullPiece)) && ((space[0] > 0 && space[1] > 0) && (space[0] < 8 && space[1] < 8))
+        valid_moves << space
+      end
+    end
+    valid_moves
 
   end
 end
