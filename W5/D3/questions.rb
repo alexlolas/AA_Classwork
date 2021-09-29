6fec8e732a5d53c944cae418f69587adaca06a7a
Hendrick Simon Ramos <hendricksimonr@gmail.com>
2021-09-29 18:16:30 -0400

    Create Users, Questions, QuestionFollows, QuestionsDatabase classes

---
+++ W5/D3/questions.rb
@@ -0,0 +1,73 @@
+require 'sqlite3'
+require 'singleton'
+
+class QuestionsDatabase < SQLite3::Database 
+   include Singleton
+
+  def initialize 
+    super('questions.db')
+    self.type_translation = true   # all data you get back returned the same
+    self.results_as_hash = true  # results given back as hash (column is key, value is the value(s) of the column)
+  end
+end
+
+class Users 
+  attr_accessor :fname, :lname, :id
+
+  def self.find_by_id(id)
+    row = QuestionsDatabase.instance.execute("SELECT * FROM users WHERE id=#{id}")
+    data = row[0]
+    Users.new(data)    
+  end 
+
+  def initialize(hash)
+    @id = hash['id']
+    @first_name = hash['fname']
+    @last_name = hash['lname']
+  end 
+end
+
+
+class Questions 
+  attr_accessor :title, :body, :id
+
+  def self.find_by_id(id)
+    row = QuestionsDatabase.instance.execute("SELECT * FROM questions WHERE id=#{id}")
+    data = row[0]
+    Questions.new(data)    
+  end 
+
+  def initialize(hash)
+    @id = hash['id']
+    @title = hash['title']
+    @body = hash['body']
+    @user_id = hash['user_id']
+  end 
+end
+
+class QuestionFollows 
+  attr_accessor :id, :user_id, :questions_id
+
+  def self.find_by_id(id)
+    row = QuestionsDatabase.instance.execute("SELECT * FROM question_follows WHERE id=#{id}")
+    data = row[0]
+    QuestionFollows.new(data)    
+  end 
+
+  def initialize(hash)
+    @id = hash['id']
+    @user_id = hash['users_id']
+    @question_id = hash['questions_id']
+  end 
+end
+
+class Replies
+  def self.find_by_id
+  end
+end 
+
+class QuestionLikes
+  def self.find_by_id
+  end
+end 
+
