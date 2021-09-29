afc3b2cbf27e554eaf2f7b6965410881c7b85208
Hendrick Simon Ramos <hendricksimonr@gmail.com>
2021-09-29 14:47:21 -0400

    Finish replies table

--- W5/D3/import_db.sql
+++ W5/D3/import_db.sql
@@ -8,9 +8,9 @@ CREATE TABLE questions (
   id INTEGER PRIMARY KEY,
   title TEXT NOT NULL,
   body TEXT NOT NULL,
-  users_id INTEGER NOT NULL,
+  user_id INTEGER NOT NULL,
 
-  FOREIGN KEY (users_id) REFERENCES users(id)
+  FOREIGN KEY (user_id) REFERENCES users(id)
 );
 
 CREATE TABLE question_follows (
@@ -23,12 +23,14 @@ CREATE TABLE question_follows (
 );
 
 CREATE TABLE replies (
-  reply_id INTEGER PRIMARY KEY,
-  question_reply TEXT NOT NULL,
-  reply_to_reply REFERENCES replies(reply_id),
-  questions_id INTEGER NOT NULL,
-  users_id INTEGER NOT NULL,
+  id INTEGER PRIMARY KEY,
+  reply TEXT NOT NULL,
+  parent_reply_id INTEGER NOT NULL,
+  question_id INTEGER NOT NULL,
+  user_id INTEGER NOT NULL,
   
-  FOREIGN KEY (users_id) REFERENCES users(id),
-  FOREIGN KEY (questions_id) REFERENCES questions(id)
+  FOREIGN KEY (parent_reply_id) REFERENCES replies(id),
+  FOREIGN KEY (user_id) REFERENCES users(id),
+  FOREIGN KEY (question_id) REFERENCES questions(id)
 );
+
