1e27d54c899029447919c754768d5dcc86a4a980
Hendrick Simon Ramos <hendricksimonr@gmail.com>
2021-09-29 14:35:36 -0400

    create tables

--- W5/D3/import_db.sql
+++ W5/D3/import_db.sql
@@ -1,1 +1,34 @@
-a
+CREATE TABLE users (
+  id INTEGER PRIMARY KEY,
+  fname TEXT NOT NULL,
+  lname TEXT NOT NULL
+);
+
+CREATE TABLE questions (
+  id INTEGER PRIMARY KEY,
+  title TEXT NOT NULL,
+  body TEXT NOT NULL,
+  users_id INTEGER NOT NULL,
+
+  FOREIGN KEY (users_id) REFERENCES users(id)
+);
+
+CREATE TABLE question_follows (
+  id INTEGER PRIMARY KEY,
+  users_id INTEGER NOT NULL,
+  questions_id INTEGER NOT NULL,
+
+  FOREIGN KEY (users_id) REFERENCES users(id),
+  FOREIGN KEY (questions_id) REFERENCES questions(id)
+);
+
+CREATE TABLE replies (
+  reply_id INTEGER PRIMARY KEY,
+  question_reply TEXT NOT NULL,
+  reply_to_reply REFERENCES replies(reply_id),
+  questions_id INTEGER NOT NULL,
+  users_id INTEGER NOT NULL,
+  
+  FOREIGN KEY (users_id) REFERENCES users(id),
+  FOREIGN KEY (questions_id) REFERENCES questions(id)
+);
