8c4e283c67f0975d8e0fe8777e256bc675ef7211
alexlolas <alexanderlolas@yahoo.com>
2021-09-30 17:13:36 -0700

    Finished Associations

--- W5/D4/skeleton 3/app/models/course.rb
+++ W5/D4/skeleton 3/app/models/course.rb
@@ -1,2 +1,21 @@
+# == Schema Information
+#
+# Table name: courses
+#
+#  id            :bigint           not null, primary key
+#  name          :string
+#  prereq_id     :integer
+#  instructor_id :integer
+#  created_at    :datetime         not null
+#  updated_at    :datetime         not null
+#
 class Course < ApplicationRecord
+  has_many :enrollments,
+    primary_key: :id,
+    foreign_key: :course_id,
+    class_name: :Enrollment
+
+  has_many :enrolled_students,
+    through: :enrollments,
+    source: :user
 end
