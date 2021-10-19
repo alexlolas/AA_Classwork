fdc77cb62d77d553c729bbd64c85a0f78469f48a
alexlolas <alexanderlolas@yahoo.com>
2021-10-19 15:37:50 -0700

    Phase two

--- W8/D1/JavaScriptDay1/phase_2_enumerable.js
+++ W8/D1/JavaScriptDay1/phase_2_enumerable.js
@@ -1,10 +1,9 @@
 Array.prototype.myEach = function(callback) {
-  let new_arr = []
+  let new_arr = this
   for (let i = 0; i < this.length; i++) {
     let element = this[i];
-    new_arr.push(callback(element));
+    callback(element);
   }
-  return new_arr; 
 }
 
 //helper method: 
@@ -12,3 +11,19 @@ let callback = function banana(params) {
   console.log(params);
 }
 
+
+
+
+Array.prototype.myMap = function(callback) {
+  let new_arr = []
+  for (let i = 0; i < this.length; i++) {
+    let element = this[i];
+    new_arr.push(callback(element));
+  }
+  return new_arr;
+}
+
+
+Array.prototype.myReduce = function(callback, [optional]) {
+  this.unshift(optional).myEach(callback);
+}
