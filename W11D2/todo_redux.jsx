a00ca35d6b586e7fc8697f63477a82db2ee95d9e
alexlolas <alexanderlolas@yahoo.com>
2021-11-10 12:39:43 -0800

    Adding React

--- W11D2/todo_redux.jsx
+++ W11D2/todo_redux.jsx
@@ -3,6 +3,8 @@ import React from 'react';
 import configStore from './frontend/store/store';
 import { receiveTodos, receiveTodo, removeTodo } from './frontend/actions/todo_action'
 import { receiveSteps, receiveStep, removeStep} from './frontend/actions/step_actions'
+import App from "./frontend/components/app"
+import Root from "./frontend/components/root"
 
 document.addEventListener("DOMContentLoaded", () => {
   const store = configStore();
@@ -13,6 +15,10 @@ document.addEventListener("DOMContentLoaded", () => {
   window.receiveSteps = receiveSteps
   window.receiveStep = receiveStep
   window.removeStep = removeStep
+
+  const root = document.getElementById("root")
+
+  ReactDOM.render(<Root store={store} />, root)
 
  
 });
