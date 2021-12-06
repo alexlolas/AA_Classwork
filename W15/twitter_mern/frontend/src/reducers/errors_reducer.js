6c674d68e04ff3cd6a3f2d9872165e3983e4ddeb
alexlolas <alexanderlolas@yahoo.com>
2021-12-06 01:33:52 -0800

    finish mern

---
+++ W15/twitter_mern/frontend/src/reducers/errors_reducer.js
@@ -0,0 +1,10 @@
+import { combineReducers } from 'redux';
+
+import SessionErrorsReducer from './session_errors_reducer';
+
+const errorReducer = combineReducers({
+  session: SessionErrorsReducer
+});
+
+export default errorReducer
+
