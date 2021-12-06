52304479432676700780dda3ced56f303915c31b
alexlolas <alexanderlolas@yahoo.com>
2021-12-05 21:30:07 -0800

    added login routes

--- W15/twitter_mern/app.js
+++ W15/twitter_mern/app.js
@@ -1,13 +1,20 @@
 const mongoose = require('mongoose');
 const express = require("express");
 const app = express();
+const users = require("./routes/api/users");
+const tweets = require("./routes/api/tweets");
 const db = require('./config/keys').mongoURI;
+const bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.get("/", (req, res) => res.send("Hello World"));
+app.use("/api/users", users);
+app.use("/api/tweets", tweets);
 mongoose
   .connect(db, { useNewUrlParser: true })
   .then(() => console.log("Connected to MongoDB successfully"))
   .catch(err => console.log(err));
 
-app.get("/", (req, res) => res.send("Hello World"));
 
 const port = process.env.PORT || 5000;
 
