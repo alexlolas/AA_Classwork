75619ee438797e533d0e07086b764419a41268e4
alexlolas <alexanderlolas@yahoo.com>
2021-12-05 22:16:58 -0800

    added tweet validations

--- W15/twitter_mern/routes/api/users.js
+++ W15/twitter_mern/routes/api/users.js
@@ -2,9 +2,18 @@ const express = require("express");
 const router = express.Router();
 const bcrypt = require('bcryptjs');
 const User = require('../../models/User');
-
+const keys = require('../../config/keys')
+const jwt = require('jsonwebtoken')
+const passport = require("passport")
+const validateRegisterInput = require('../../validation/register')
+const validateLoginInput = require('../../validation/login')
 router.get("/test", (req, res) => res.json({ msg: "This is the users route" }));
 router.post('/register', (req, res) => {
+  const { errors, isValid } = validateRegisterInput(req.body)
+
+  if(!isValid) {
+    return res.status(400).json(errors)
+  }
   // Check to make sure nobody has already registered with a duplicate email
   User.findOne({ email: req.body.email })
     .then(user => {
@@ -33,6 +42,12 @@ router.post('/register', (req, res) => {
 })
 
 router.post('/login', (req, res) => {
+  const { errors, isValid } = validateLoginInput(req.body)
+
+  if (!isValid) {
+    return res.status(400).json(errors)
+  }
+
   const email = req.body.email
   const password = req.body.password
 
@@ -45,7 +60,24 @@ router.post('/login', (req, res) => {
       bcrypt.compare(password, user.password)
         .then(isMatch => {
           if(isMatch){
-            res.json({ msg: 'Sucess!' })
+            const payload = {
+              id: user.id,
+              handle: user.handle,
+              email: user.email
+            }
+            jwt.sign(
+              payload,
+              keys.secretOrKey,
+              {
+                expiresIn: 3600
+              },
+              (err, token) => {
+                res.json({
+                  success: true,
+                  token: "Bearer " + token
+                })
+              }
+            )
           } else {
             return res.status(400).json({ password: "Incorrect Password" })
           }
