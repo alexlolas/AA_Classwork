f39612fb3a7c71eb39eb2d9fae95e5237b361551
alexlolas <alexanderlolas@yahoo.com>
2021-12-06 01:41:54 -0800

    pls

--- W15/twitter_mern/app.js
+++ W15/twitter_mern/app.js
@@ -6,6 +6,8 @@ const tweets = require("./routes/api/tweets");
 const db = require('./config/keys').mongoURI;
 const bodyParser = require('body-parser');
 const passport = require('passport');
+const path = require('path');
+
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(passport.initialize());
@@ -23,3 +25,10 @@ const port = process.env.PORT || 5000;
 
 
 app.listen(port, () => console.log(`Server is running on port ${port}`));
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('frontend/build'));
+  app.get('/', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'));
+  })
+}
